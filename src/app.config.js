14967dfbc20a4e6ad6841e6e99df7cc5be5d2ae8
唐磊 <tanglei@fiture.com>
2021-09-08 11:11:55 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,9 @@
+export default {
+  pages: ["pages/index/index", "pages/wxParse/wxParse"],
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+};
---
+++ src/pages/wxParse/wxParse.config.js
@@ -0,0 +1,10 @@
+export default {
+  navigationBarBackgroundColor: "#ffffff",
+  navigationBarTextStyle: "black",
+  navigationBarTitleText: "WxParse 使用示例",
+  backgroundColor: "#eeeeee",
+  backgroundTextStyle: "light",
+  usingComponents: {
+    tab: "../../components/tab/tab", // 书写第三方组件的相对路径
+  },
+};
